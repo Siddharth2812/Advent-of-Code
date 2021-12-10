c6757f22bda70383ff45d6010c204459bfe19150
Siddharth Reddy Anthireddy <90183587+Siddharth2812@users.noreply.github.com>
2021-12-11 00:28:31 +0530

    Update part-1.py

--- Day-1/part-1.py
+++ Day-1/part-1.py
@@ -1,12 +1,11 @@
-import sys
-n = sys.stdin.readlines()
-s = []
-for i in n:
-    a = i.replace('\n','')
-    s.append(int(a))
-c = 0
-a = len(s)
-for i in range(len(s)-1):
-    if s[i] < s[i+1]:
-        c+=1
-print(c)
+n = int(input())
+count = 0
+while True:
+    try:
+        m = int(input())
+        if m>n:
+            count+=1
+        n = m
+    except:
+        break
+print(count)
